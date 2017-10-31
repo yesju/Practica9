d65c1ba78db18aa2fb7b07f875b2b951b1306f5e
omx1 <omx1@YESICA>
2017-10-30 23:02:29 -0600

    Add project files.

---
+++ Prac8/Prac8/SQLAzure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac8
+{
+   public interface SQLAzure
+    {
+        Task<bool> Authenticate();
+    }
+}
